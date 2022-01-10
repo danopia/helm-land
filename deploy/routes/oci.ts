a62d4dbc93401192af75fa0017de795819a480b4
Daniel Lamando <dan@danopia.net>
2022-01-10 01:46:53 +0100

    401 lol

--- deploy/routes/oci.ts
+++ deploy/routes/oci.ts
@@ -1,6 +1,7 @@
 export async function renderOciManifest(requestUrl: URL, ownerId: string, chartId: string, version: string, authorization: string | null) {
   if (!authorization) {
     return new Response(`{"errors":[{"code":"UNAUTHORIZED","message":"authentication required"}]}`, {
+      status: 401,
       headers: {
         'content-type': 'application/json',
         'www-authenticate': `Bearer realm="${requestUrl.origin}/v2/token",service="${requestUrl.hostname}",scope="repository:${ownerId}/${chartId}:pull"`,
